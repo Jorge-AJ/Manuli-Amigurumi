a029abbec8defe3ae009df5ad856b64318042598
Jorge-AJ <jorgeajunior01@gmail.com>
2024-09-06 19:58:52 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    {
+        titulo: "Amigurumi",
+        descricao: "Amigurumis são pequenas e adoráveis criaturas feitas à mão com a técnica de crochê.",
+        link: "https://www.instagram.com/manuca_cunha/",
+        tags: ["crochê", "artesanato"],
+        autor: "Manuca Cunha",
+        dataCriacao: "2023-11-20",
+        dificuldade: "fácil"
+    }, 
+{
+    titulo: "Vakinha Kratos",
+        descricao: "Vaka mais fashion e vingativa do mundo.",
+        link: "https://www.instagram.com/manuca_cunha/",
+        tags: ["deuses", "artesanato"],
+        autor: "Manuca Cunha",
+        dataCriacao: "2023-11-20",
+        dificuldade: "fácil"
+}, 
+{
+    titulo: "Psicopato",
+        descricao: "Tão fofo que pode matar quack quack.",
+        link: "https://www.instagram.com/manuca_cunha/",
+        tags: ["crochê", "artesanato"],
+        autor: "Manuca Cunha",
+        dataCriacao: "2023-11-20",
+        dificuldade: "fácil"
+}
+]
